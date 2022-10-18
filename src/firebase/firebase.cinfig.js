993c650070c1297f3eb2ef2e3bc9d0478df687b8
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-19 04:50:33 +0600

    signin

---
+++ src/firebase/firebase.cinfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDgok1tU7b3Hk6jKpqi2d3D7CU2gtvgaEY",
+    authDomain: "ema-jon-smile.firebaseapp.com",
+    projectId: "ema-jon-smile",
+    storageBucket: "ema-jon-smile.appspot.com",
+    messagingSenderId: "383860282789",
+    appId: "1:383860282789:web:0633d84905fcfb6ea9444c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
