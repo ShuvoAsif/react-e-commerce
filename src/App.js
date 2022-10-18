05f24d455ea282fbcc6c67a2d5fbce8d3d28d5a2
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-18 20:10:38 +0600

    router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([{
+    path: '/',
+    Element: <Main></Main>
+    children: [
+
+    ]
+
+  },
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/layout/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/header/header';
+import Shop from '../components/shop/shop';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Shop></Shop>
+        </div>
+    );
+};
+
+export default Main;
