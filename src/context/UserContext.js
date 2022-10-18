073b25f64fea71d5348a429b35e95508fabd67de
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-19 05:10:57 +0600

    api

---
+++ src/context/UserContext.js
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react';
+
+export const AuthContext = createContext();
+const authInfo = {}
+
+const UserContext = ({ children }) => {
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default UserContext;
