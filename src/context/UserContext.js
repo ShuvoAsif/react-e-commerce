e515bd346ff6e3107425cbb07e34cbf2dee7d3fc
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-20 03:42:12 +0600

    userContext

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,9 +1,43 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import app from '../firebase/firebase.cinfig'
 
 export const AuthContext = createContext();
-const authInfo = {}
+const auth = getAuth(app);
+
 
 const UserContext = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('current User inside state change', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => unSubscribe();
+
+    }, [])
+
+    const authInfo = { user, loading, createUser, signIn, logOut }
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
