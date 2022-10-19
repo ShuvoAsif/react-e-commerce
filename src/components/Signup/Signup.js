32712cfa8b52e77b7973e77d90785194b84477c9
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-10-20 03:59:21 +0600

    signup

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/UserContext';
 
 const Signup = () => {
+
+    const [error, setError] = useState(null);
+    const { createUser } = useContext(AuthContext);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+
+        if (password.length < 6) {
+            setError('Password should be 6 characters or more.');
+            return;
+        }
+
+        if (password !== confirm) {
+            setError('Your Password did not match');
+            return;
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+            })
+            .catch(error => console.error(error));
+    }
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" required />
@@ -21,7 +51,7 @@ const Signup = () => {
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
             <p>Already Have an Account <Link to='/login'>Login</Link></p>
-            <p className='text-error'>{ }</p>
+            <p className='text-error'>{error}</p>
         </div>
     );
 };
