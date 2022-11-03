5d8eedc8353879c74f0ba98a90721c7775568371
ShuvoAsif <asif.shuvo.learning@gmail.com>
2022-11-04 05:28:54 +0600

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <Link to="/">Home</Link>
+                <Link to="/">Shop</Link>
                 <Link to="/orders">Order</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">About</Link>
